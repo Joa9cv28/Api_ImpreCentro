496e868c6c87498ad4f32bf7419605fc7849c370
joa9cv28 <joaninecordov@gmail.com>
2025-02-03 18:08:04 -0600

    configuracion vieja de app.py

--- app.py
+++ app.py
@@ -1,116 +1,144 @@
-from datetime import datetime
-from fastapi import APIRouter, FastAPI, File, HTTPException, UploadFile
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, HTTPException, APIRouter, File, UploadFile
 from fastapi.responses import FileResponse
-from pathlib import Path
-from pydantic import BaseModel
-import boto3
-from botocore.exceptions import BotoCoreError, ClientError
-import clases
-import conexion
-import os
 import shutil
+import os
+import conexion
+import clases
+from datetime import datetime
+from fastapi.middleware.cors import CORSMiddleware
 
-# Configuración de AWS S3
-BUCKET_NAME = "mi-app-web-bucket"
-
-# Cliente de S3
-session = boto3.Session(profile_name="default")
-s3_client = boto3.client("s3", region_name="us-east-1")
-
-# Inicializar FastAPI
 app = FastAPI()
+# Crear un router
 api_router = APIRouter(prefix="/api")
 
-# Carpeta local
+# Carpeta donde se guardarán los archivos
 UPLOAD_DIRECTORY = "documentos"
-UPLOAD_FOLDER = Path("test")
-UPLOAD_FOLDER.mkdir(exist_ok=True)
 
-# CORS
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Cambiar a dominios específicos en producción
+    allow_origins='*', ##IMPORTANTE
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Rutas del API
-@app.exception_handler(Exception)
+# Asegúrate de que la carpeta exista
+os.makedirs(UPLOAD_DIRECTORY, exist_ok=True)
+
+@app.exception_handler(Exception)                     #TRY-CATCH
 async def global_exception_handler(request, exc):
+    # Registra la excepción o realiza acciones de manejo
+    #imprimimos y devolvemos
     print(f"Error interno: {exc}")
     raise HTTPException(status_code=500, detail="Error interno del servidor")
 
-@api_router.get("/")
+@api_router.get('/') #RUTA RAIZ
 def read_root():
-    return {"welcome": "Welcome to my api"}
+    return {"welcome":"Welcome to my api"}
 
-@api_router.get("/usuarios")
+@api_router.get('/usuarios')
 def read_usuarios():
-    registros = conexion.devolverRegistros("usuarios")
-    usuarios = [clases.Usuario(
-        usu_id=registro[0],
-        usu_correo=registro[1],
-        usu_nombre=registro[3],
-        usu_codigo=registro[4]
-    ) for registro in registros]
-    return clases.Respuesta(success=True, message="All ok", data=usuarios)
+    registros = conexion.devolverRegistros('usuarios')
+    usuarios = []
+    for registro in registros:
+        usuarios.append(clases.Usuario(usu_id=registro[0], usu_correo=registro[1], usu_nombre=registro[3], usu_codigo=registro[4]))
+    return clases.Respuesta(success=True, message='All ok', data=usuarios)
 
-@api_router.post("/upload")
-async def upload_file(file: UploadFile = File(...)):
-    """Sube un archivo al bucket S3."""
-    if not file.content_type.startswith("image/"):
-        raise HTTPException(status_code=400, detail="Solo se permiten archivos de imagen")
+@api_router.get('/archivos')
+def read_archivos():
+    registros = conexion.devolverArchivos()
+    archivos = []
+    for registro in registros:
+        archivos.append(clases.Archivo(arc_id=registro[0], arc_nombre=registro[1], arc_ruta=registro[2], arc_tiempo=registro[3], arc_fecha=registro[4], usuario=clases.Usuario(usu_id=registro[6], usu_correo=registro[7], usu_nombre=registro[9], usu_codigo=registro[10])))
+    return clases.Respuesta(success=True, message='All ok', data=archivos)
 
-    try:
-        s3_client.upload_fileobj(
-            file.file,
-            BUCKET_NAME,
-            file.filename,
-            ExtraArgs={"ContentType": file.content_type},
-        )
-        return {"message": f"Archivo {file.filename} subido correctamente a S3"}
-    except (BotoCoreError, ClientError) as e:
-        print(f"Error subiendo archivo: {str(e)}")
-        raise HTTPException(status_code=500, detail="Error al subir el archivo")
+@api_router.post('/usuario')
+def registrarUsuarios(usuario:clases.Usuario):
+    resultado = conexion.registrarUsuario(usuario)
+    return clases.Respuesta(success=True, message='All ok', data=resultado)
 
-@api_router.get("/download-all")
-async def download_all_files():
-    """Descarga todos los archivos del bucket S3 a una carpeta local."""
-    folder_path = Path("documentos")
-    folder_path.mkdir(exist_ok=True)
+@api_router.post('/archivo')
+def registrarArchivo(archivo:clases.Archivo):
+    archivo.arc_ruta = archivo.arc_ruta.replace("\\", "\\\\")
+    print(archivo.arc_ruta)
+    resultado = conexion.registrarArchivo(archivo)
+    return clases.Respuesta(success=True, message='All ok', data=resultado)
 
-    try:
-        response = s3_client.list_objects_v2(Bucket=BUCKET_NAME)
-        objects = response.get("Contents", [])
-        for obj in objects:
-            key = obj["Key"]
-            local_file_path = folder_path / key
-            s3_client.download_file(BUCKET_NAME, key, str(local_file_path))
+@api_router.post("/subirArchivo")
+async def subirArchivo(file: UploadFile):
+    filename = file.filename
+    file_extension = os.path.splitext(filename)[1]
+    now = datetime.now()
+    filename = now.strftime("%Y%m%d%H%M%S")+str(now.microsecond)+file_extension
+    file_location = os.path.join(UPLOAD_DIRECTORY, filename)
+    with open(file_location, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    print(datetime.now())
+    return clases.Respuesta(success=True, message='All ok', data={"location": file_location})
+    #return {"file_extension": file_extension,"filename": file.filename, "location": file_location}
 
-        return {"message": "Archivos descargados correctamente", "files": [obj["Key"] for obj in objects]}
-    except Exception as e:
-        print("Error:", str(e))
-        raise HTTPException(status_code=500, detail="Error al descargar archivos")
 
-@api_router.put("/rename")
-async def rename_file(request: BaseModel):
-    """Renombra un archivo en el bucket S3."""
-    old_name = request.old_name
-    new_name = request.new_name
-    if not new_name.lower().endswith(".jpg"):
-        raise HTTPException(status_code=400, detail="El archivo debe tener extensión .jpg")
-    try:
-        s3_client.copy_object(
-            Bucket=BUCKET_NAME,
-            CopySource={"Bucket": BUCKET_NAME, "Key": old_name},
-            Key=new_name,
-        )
-        s3_client.delete_object(Bucket=BUCKET_NAME, Key=old_name)
-        return {"message": f"Archivo {old_name} renombrado a {new_name}"}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error al renombrar el archivo: {str(e)}")
+@api_router.get("/descargarArchivo/{id}")
+async def get_file(id: int):
+    archivo = conexion.buscarArchivo(id)
+    file_path = archivo[0][2].replace("\\", "\\\\")
+    if os.path.exists(file_path):
+        print(file_path)
+        return FileResponse(file_path, filename=archivo[0][1], media_type='application/octet-stream')
+    return {"error": "Archivo no encontrado"} 
 
-# Incluir router en la aplicación principal
+@api_router.post('/login')
+def loginUsuario(usuario:clases.Login):
+    resultado = conexion.loginUsuario(usuario)
+    print(resultado)
+    print()
+    if (resultado['success'] == True):
+        return clases.Respuesta(success=True, message='All ok', data=None)
+    elif (resultado['success'] == False):
+        if (resultado['message'] == 'Usuario no encontrado.' or resultado['message'] == 'Contraseña incorrecta.'):
+            return clases.Respuesta(success=False, message='Invalid password or email', data=None)
+        else:
+            return clases.Respuesta(success=False, message='Please contact an administrator', data=None)
+    return clases.Respuesta(success=False, message='Intern problems, please contact an administrator', data=None)
+
+@api_router.delete('/usuario')
+def borrarUsuario(usuario:clases.Usuario):
+    resultado = conexion.borrarUsuario(usuario)
+    return clases.Respuesta(success=True, message='All ok', data=resultado)
+
+@api_router.delete('/archivo')
+def borrarArchivo(archivo:clases.Archivo):
+    resultado = conexion.borrarArchivo(archivo)
+    return clases.Respuesta(success=True, message='All ok', data=resultado)
+
+@api_router.put('/usuario')
+def actualizarUsuario(usuario:clases.Usuario):
+    resultado = conexion.actualizarUsuario(usuario)
+    return clases.Respuesta(success=True, message='All ok', data=resultado)
+
+# Incluir el router en la aplicación principal
 app.include_router(api_router)
+
+
+# {
+# stauts: true,
+# message: "All ok"
+# data: {
+# }}
+
+# {
+# stauts: true,
+# message: "All ok",
+# data: {
+# id: 20,
+# correo: "ASDASD",
+# nombre: "ASDASDAS",
+# password: "ASDASD",
+# codigo: 12313122
+# }
+# }
